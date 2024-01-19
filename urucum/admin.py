9cb78d66bf05aa0f5e27c19479ed35fe73e8c0c4
Programação em Python 2023.5 <36129532023.5@senacrjedu.local>
2024-01-19 16:07:33 -0300

    atualizando sessão pratos

--- urucum/admin.py
+++ urucum/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from cardapio.admin import ListandoPratos
+from cardapio.models import Prato
 
 # Register your models here.
+
--- urucum/models.py
+++ urucum/models.py
@@ -1,3 +1,6 @@
 from django.db import models
+from cardapio.models import Prato
 
 # Create your models here.
+
+
